d60623a46b70aa98fe17dfa64ba5350566e89a8b
Sluchaino <145386647+Sluchaino@users.noreply.github.com>
2024-05-10 19:47:51 +0300

    User, IUserRepository and IRepository<T>
    Сущность User и интерфейсы IUserRepository, IRepository<T>

---
+++ DataAccessLayer/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interfaces
+{
+    // Интерфейс для работы с моделью Пользователя. Он вообще нужен? 
+    
+    public interface IUserRepository : IRepository<User>
+    {
+        
+    }
+}
---
+++ DataAccessLayer/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Models;
+
+namespace DataAccessLayer.Models
+{
+    public class User
+    {
+        public string UserId { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public int Elo { get; set; }
+    }
+}
