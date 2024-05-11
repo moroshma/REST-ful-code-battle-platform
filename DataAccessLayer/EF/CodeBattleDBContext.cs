c97c1ea8201d885dbeab1e815fbfad21be1af0f5
moroshma <artyomspvzkrik@gmail.com>
2024-05-11 20:12:06 +0300

    add migration

--- DataAccessLayer/EF/CodeBattleDBContext.cs
+++ DataAccessLayer/EF/CodeBattleDBContext.cs
@@ -5,22 +5,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using  Npgsql.EntityFrameworkCore.PostgreSQL;
 using DataAccessLayer.Models;
 
 namespace DataAccessLayer.EF
 {
     public class CodeBattleDBContext : DbContext
     {
+        
+        private readonly bool isMigration = false;
+       public CodeBattleDBContext()
+        {
+            isMigration = true;
+        }
         public CodeBattleDBContext(DbContextOptions<CodeBattleDBContext> options): base(options)
         {
             Database.EnsureCreated();
+            
         }
         public DbSet<User> Users { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseNpgsql("Server=localhost;Username=postgres;Database=postgres", b => b.MigrationsAssembly("PresentationLayer"));
+                optionsBuilder.UseNpgsql("Server=localhost;Username=postgres;Database=postgres;Port=5432;Password=root;", b => b.MigrationsAssembly("PresentationLayer"));
+            }
+            if ( isMigration )
+            {
+                optionsBuilder.UseNpgsql( "Server=localhost;Username=postgres;Database=postgres;Port=5432;Password=root;" );
             }
         }
     }
--- PresentationLayer/Program.cs
+++ PresentationLayer/Program.cs
@@ -8,7 +8,7 @@ using Npgsql;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<CodeBattleDBContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnetction")));
+builder.Services.AddDbContext<CodeBattleDBContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,6 +19,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
+    
 {
     app.UseSwagger();
     app.UseSwaggerUI();
