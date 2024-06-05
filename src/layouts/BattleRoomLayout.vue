32341f28ccb0abe90dcbe5c780081db8cebef6c9
Egor <jdmjapanese69@gmail.com>
2024-06-05 21:48:45 +0300

    ace editor from npn pack install

--- src/layouts/BattleRoomLayout.vue
+++ src/layouts/BattleRoomLayout.vue
@@ -1,13 +1,17 @@
 <script setup>
-import { onMounted } from 'vue';
+import { VAceEditor } from 'vue3-ace-editor';
+import ace from 'ace-builds';
+import modeGoUrl from 'ace-builds/src-noconflict/mode-golang?url';
+import themeDraculaUrl from 'ace-builds/src-noconflict/theme-dracula?url';
+import {onMounted,ref} from 'vue'
 
-onMounted(()=>{
-    const editor = ace.edit("editor");
-    console.log(editor);
-    editor.setTheme("ace/theme/dracula");
-    editor.session.setMode("ace/mode/golang");
-    editor.setShowPrintMargin(false);
-})
+const aceRef = ref(null);
+ace.config.setModuleUrl('ace/theme/dracula', themeDraculaUrl);
+ace.config.setModuleUrl('ace/mode/golang', modeGoUrl);
+onMounted(() => {
+        console.log(aceRef.value.getAceInstance());
+        aceRef.value.getAceInstance().setFontSize(20)
+   })
 </script>
 
 <template>
@@ -15,26 +19,22 @@ onMounted(()=>{
 
         <div class="task">Программа должна вывести на экран строку "Hello, World!" на языке программирования Go.</div>
         <div class="editor_container">
-            <div id="editor"></div>
+            <v-ace-editor ref="aceRef" :print-margin="false" v-model:value="content" @init="editorInit" lang="golang" theme="dracula"
+                style="height: 90vh" :options="{ useWorker: true }"/>
             <el-button plain type="warning"> Проверить </el-button>
         </div>
     </div>
 </template>
 
 <style scoped>
-    .editor_container{
-        position: relative;
-        width: 60%;
-        height: 500px;
-    }
+.editor_container {
+    position: relative;
+    width: 60%;
+    height: 500px;
+}
 
-    #editor{
-        width: 100%;
-        height: 90vh; 
-    }
-
-    .container{
-        display: flex;
-        justify-content: space-between;
-    }
+.container {
+    display: flex;
+    justify-content: space-between;
+}
 </style>
