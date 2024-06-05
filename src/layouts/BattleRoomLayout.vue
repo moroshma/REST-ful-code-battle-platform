55fdd0343c385309cfd246b09ccbec8dde83281a
Egor <jdmjapanese69@gmail.com>
2024-06-05 17:13:51 +0300

    ace-editor added in battleRoom

--- src/layouts/BattleRoomLayout.vue
+++ src/layouts/BattleRoomLayout.vue
@@ -1,9 +1,40 @@
 <script setup>
+import { onMounted } from 'vue';
 
+onMounted(()=>{
+    const editor = ace.edit("editor");
+    console.log(editor);
+    editor.setTheme("ace/theme/dracula");
+    editor.session.setMode("ace/mode/golang");
+    editor.setShowPrintMargin(false);
+})
 </script>
 
 <template>
-    <div>
-        Room
+    <div class="container">
+
+        <div class="task">Программа должна вывести на экран строку "Hello, World!" на языке программирования Go.</div>
+        <div class="editor_container">
+            <div id="editor"></div>
+            <el-button plain type="warning"> Проверить </el-button>
+        </div>
     </div>
 </template>
+
+<style scoped>
+    .editor_container{
+        position: relative;
+        width: 60%;
+        height: 500px;
+    }
+
+    #editor{
+        width: 100%;
+        height: 90vh; 
+    }
+
+    .container{
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
